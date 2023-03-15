af1a6e466323c9cb1b5730567cc06848465f1053
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-14 18:55:58 -0700

    Did some changes on spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,27 +1,29 @@
-const spinner1 = function() {
+let phrase = "Oh yes, it is totally okey.";
 
-  setTimeout(() => {
-    process.stdout.write(`\r|   `);
-  }, 100);
+const spinner1 = function(letters) {
+  for (let letter of letters) {
+    setTimeout(() => {
+      process.stdout.write(`\r| ${letter}  `);
+    }, 100);
 
 
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 300);
+    setTimeout(() => {
+      process.stdout.write('\r/    ');
+    }, 300);
 
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 500);
+    setTimeout(() => {
+      process.stdout.write('\r-  ');
+    }, 500);
 
-  setTimeout(() => {
-    // Need to escape the backslash since it's a special character.
-    process.stdout.write('\r\\   ');
-  }, 700);
-
+    setTimeout(() => {
+      // Need to escape the backslash since it's a special character.
+      process.stdout.write('\r\\  ');
+    }, 700);
+  }
   // ... fill in the rest yourself ...
 };
 
-spinner1();
+spinner1(phrase);
 
 
 
