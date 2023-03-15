53ebe930df641a74c897b7e26f22ebd46f2ecfe6
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-14 18:35:10 -0700

    Adding spinner1.js

---
+++ spinner1.js
@@ -0,0 +1,28 @@
+const spinner1 = function() {
+
+  setTimeout(() => {
+    process.stdout.write(`\r|   `);
+  }, 100);
+
+
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300);
+
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   ');
+  }, 700);
+
+  // ... fill in the rest yourself ...
+};
+
+spinner1();
+
+
+
+
